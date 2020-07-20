a7cb1c643589b5345e6178c10702ae7d805c372c
LSparkzwz <costadanilo@protonmail.com>
2020-07-21 00:08:50 +0200

    bugfix

--- lambda functions/lambda_function.py
+++ lambda functions/lambda_function.py
@@ -1,4 +1,5 @@
 import trip_updates_feed as tuf
+import stops
 import cassandra_session_init as csi
 import time
 
@@ -8,8 +9,8 @@ def lambda_handler(event, context):
     current_time = int(time.time())
     cassandra_session = csi.cassandra_session_init()
     feed = tuf.update_feed()
-    stops = tuf.get_stops(feed,current_time)
-    tuf.insert_stops(stops, current_time, cassandra_session)
+    stop_feed = stops.get_stops(feed,current_time)
+    stops.insert_stops(stop_feed, current_time, cassandra_session)
 
     return {
         'statusCode': 200
